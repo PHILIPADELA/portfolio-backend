aac5c6a819ce807faf8c7d86e84ba6084ca9bce1
ADELA009 <adeyekunadelola2009@gmail.com>
2025-05-17 20:32:49 +0100

    adelaaa

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 5000,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://ADELAA:adeLOLA1.9@cluster0.hsbkpfo.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Cluster0',
-  JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key',
+  JWT_SECRET: process.env.JWT_SECRET || 'rTTlluVwRcTkIkHx5cX+GNTJf85bJrZ/bFG7Q57LArQ',
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:2000'
+  CLIENT_URL: process.env.CLIENT_URL || 'https://portfolio-frontend-wheat-ten.vercel.app'
 };
