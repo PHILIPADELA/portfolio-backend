12790705c767f56c697ad7168d4e55420c298a2f
ADELA009 <adeyekunadelola2009@gmail.com>
2025-05-18 13:53:05 +0100

    continue

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -3,16 +3,24 @@ const router = express.Router();
 const commentController = require('../controllers/commentController');
 const cors = require('cors');
 
-
 router.use(cors());
 
-
+// Get all comments for a blog post
 router.get('/blog/:blogPostId/comments', commentController.getComments);
 
-
+// Create a new comment
 router.post('/blog/:blogPostId/comments', commentController.createComment);
 
-
+// Delete a comment (will also delete all replies)
 router.delete('/blog/:blogPostId/comments/:commentId', commentController.deleteComment);
+
+// Get replies for a specific comment
+router.get('/blog/:blogPostId/comments/:commentId/replies', commentController.getReplies);
+
+// Create a reply to a comment
+router.post('/blog/:blogPostId/comments/:commentId/replies', commentController.createReply);
+
+// Delete a reply
+router.delete('/blog/:blogPostId/comments/:commentId/replies/:replyId', commentController.deleteReply);
 
 module.exports = router;
