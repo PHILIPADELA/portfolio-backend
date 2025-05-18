13869262e93f9236ccbe35407901d8e6207756aa
ADELA009 <adeyekunadelola2009@gmail.com>
2025-05-18 13:58:13 +0100

    uuuu

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express');const router = express.Router();
 const commentController = require('../controllers/commentController');
 const cors = require('cors');
 
 router.use(cors());
 
 // Get all comments for a blog post
-router.get('/blog/:blogPostId/comments', commentController.getComments);
+router.get('/:blogPostId/comments', commentController.getComments);
 
 // Create a new comment
-router.post('/blog/:blogPostId/comments', commentController.createComment);
+router.post('/:blogPostId/comments', commentController.createComment);
 
 // Delete a comment (will also delete all replies)
-router.delete('/blog/:blogPostId/comments/:commentId', commentController.deleteComment);
+router.delete('/:blogPostId/comments/:commentId', commentController.deleteComment);
 
 // Get replies for a specific comment
-router.get('/blog/:blogPostId/comments/:commentId/replies', commentController.getReplies);
+router.get('/:blogPostId/comments/:commentId/replies', commentController.getReplies);
 
 // Create a reply to a comment
-router.post('/blog/:blogPostId/comments/:commentId/replies', commentController.createReply);
+router.post('/:blogPostId/comments/:commentId/replies', commentController.createReply);
 
 // Delete a reply
-router.delete('/blog/:blogPostId/comments/:commentId/replies/:replyId', commentController.deleteReply);
+router.delete('/:blogPostId/comments/:commentId/replies/:replyId', commentController.deleteReply);
 
 module.exports = router;
