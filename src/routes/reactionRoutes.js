87e8de88f36b079f67e2b22e493e829845733b59
ADELA009 <adeyekunadelola2009@gmail.com>
2025-06-13 13:52:03 +0100

    ade

--- src/routes/reactionRoutes.js
+++ src/routes/reactionRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const reactionController = require('../controllers/reactionController');
 
-router.post('/:Id', reactionController.toggleReaction);
+router.post('/:postId', reactionController.toggleReaction);
 
 module.exports = router;
