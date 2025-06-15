b698726538734b6f90941ab53faf7623d36b4e51
ADELA009 <adeyekunadelola2009@gmail.com>
2025-06-15 04:01:38 +0100

    fix: downgrade express to version 4.18.2 for compatibility; update MongoDB URI format

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 5000,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://ADELAA:adeLOLA1.9@cluster0.hsbkpfo.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Cluster0',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://ADELAA:adeLOLA1.9@cluster0.hsbkpfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   JWT_SECRET: process.env.JWT_SECRET || 'rTTlluVwRcTkIkHx5cX+GNTJf85bJrZ/bFG7Q57LArQ',
   NODE_ENV: process.env.NODE_ENV || 'production',
   CLIENT_URL: process.env.CLIENT_URL || 'https://portfolio-frontend-wheat-ten.vercel.app',
   
-  // Email configuration
+ 
   EMAIL_HOST: process.env.EMAIL_HOST || 'smtp.gmail.com',
   EMAIL_PORT: process.env.EMAIL_PORT || 587,
   EMAIL_USER: process.env.EMAIL_USER || 'adeyekunadelola0@gmail.com',
